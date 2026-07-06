b6d733e513679e647784579f8c377b59def4ef26
kanchanr2208 <267316562+kanchanr2208@users.noreply.github.com>
2026-07-06 04:12:11 +0000

    add event listeners. make equals button functional

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let finalResult = "";
+let finalResult = null;
+let variableOne = null;
+let variableTwo = null;
+let isOperator = null;
 
 function add(a, b) {
     return finalResult = (a + b);
@@ -22,4 +25,62 @@ function divide(a, b) {
     return finalResult;
 }
 
-console.log(subtract(10, 2))
+//e cha kaam nantar yeil. adding now just for funzies
+let numbersNodeList = document.querySelectorAll(".number")
+numbersNodeList.forEach(number => {
+    number.addEventListener("click", (e) => {
+        let textContent = e.target.textContent;
+        console.log(textContent)
+    })
+})
+
+let operatorsNodeList = document.querySelectorAll(".operator")
+operatorsNodeList.forEach(operator => {
+    operator.addEventListener("click", (e) => {
+        let operatorContent = e.target.textContent;
+        isOperator = operatorContent
+    })
+})
+
+let clear = document.querySelector(".clear")
+clear.addEventListener("click", () => {
+    variableOne = null;
+    variableTwo = null;
+    isOperator = null;
+})
+
+
+let equalTo = document.querySelector(".equals")
+equalTo.addEventListener("click", (e) => {
+    if ((variableOne === null && isOperator === null && variableTwo === null) || 
+        (variableOne === null && isOperator === null && variableTwo !== null) || 
+        (variableOne !== null && isOperator === null && variableTwo !== null)) {
+        finalResult = null;
+    } else if (variableOne !== null  && isOperator === null && variableTwo === null) {
+        finalResult = variableOne;
+    } else if (variableOne !== null && isOperator !== null && variableTwo === null) {
+        finalResult = variableOne;
+    } else if (variableOne !== null && isOperator !== null && variableTwo !== null) {
+        if(isOperator == "+") {
+            add(variableOne, variableTwo)
+        } else if(isOperator == "-") {
+            subtract(variableOne, variableTwo)
+        } else if (isOperator == "*") {
+            multiply(variableOne, variableTwo)
+        } else if (isOperator == "/") {
+            divide(variableOne, variableTwo)
+        } else {
+            finalResult = "error"
+        }
+    }
+    console.log(finalResult)
+})
+
+
+
+
+
+
+
+
+
